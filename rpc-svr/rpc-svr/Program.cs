bd3355b7e03d6d26c8fa83c82db7e461e8679d5a
lamborcai <lamborcai@gmail.com>
2019-02-19 17:54:03 +0800

    svr

---
+++ rpc-svr/rpc-svr/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace rpc_svr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
