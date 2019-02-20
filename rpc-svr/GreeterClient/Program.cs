208c383102d1f589ea281e34ea4b7c15c3226d72
lamborcai <lamborcai@gmail.com>
2019-02-20 14:26:34 +0800

    update grpc demo

---
+++ rpc-svr/GreeterClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Helloworld;
+
+namespace GreeterClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+
+            string user = "NBS Tech";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+
+            Console.WriteLine("Greeter reply :" + reply.Message);
+
+            var secReply = client.SayHello(new HelloRequest { Name = "lanbery" });
+            Console.WriteLine("Greeting: " + secReply.Message);
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ rpc-svr/GreeterServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Helloworld;
+
+namespace GreeterServer
+{
+    //
+    class GreeterImpl : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request,ServerCallContext context)
+        {
+            Console.WriteLine("Greeting cli:" + request.Name);
+            return Task.FromResult(new HelloReply { Message = "你好 " + request.Name });
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Greeter.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server ...");
+
+            string flag = Console.ReadKey().Key.ToString();
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
