d4314f9977ea474adcf6396649d1251e40bdc269
lamborcai <lamborcai@gmail.com>
2019-02-20 17:05:29 +0800

    修改服务端IP

--- rpc-svr/GreeterClient/Program.cs
+++ rpc-svr/GreeterClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Grpc.Core;
 using Helloworld;
 
@@ -10,22 +11,65 @@ namespace GreeterClient
 {
     class Program
     {
+        const string defHost = "127.0.0.1";
         static void Main(string[] args)
         {
+            string svrHost = defHost;
+
+            string line;
+            string patternip = @"((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d)(\.((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d)){3}";
+            string patterndomain = @"^[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$";
+            Regex regexIP = new Regex(patternip);
+            Regex domainRegex = new Regex(patterndomain);
+            bool flag = true;
+            do
+            {
+                Console.WriteLine("请输入Gprc服务端IP,或输入`c` 字符使用默认IP.");
+                try
+                {
+                    line = Console.ReadLine();
+                    if (string.IsNullOrEmpty(line)) {
+                        flag = false;
+                        continue;
+                    };
+                    if(regexIP.IsMatch(line) || domainRegex.IsMatch(line))
+                    {
+                        svrHost = line;
+                        flag = false;
+                    }
+                }
+                catch 
+                {
+                    Console.WriteLine("请输入服务IP或直接回车（Enter）使用默认IP.");
+                }
+            } while (flag);
+
             //
-            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+            string svrHostName = svrHost + ":50051";
+            Channel channel;
+            try
+            {
+                channel = new Channel(svrHostName, ChannelCredentials.Insecure);
 
-            var client = new Greeter.GreeterClient(channel);
+                Console.WriteLine("Client connecting Channel " + svrHostName + ".");
 
-            string user = "NBS Tech";
+                var client = new Greeter.GreeterClient(channel);
+                            
+                string user = "NBS Tech";
 
-            var reply = client.SayHello(new HelloRequest { Name = user });
+                var reply = client.SayHello(new HelloRequest { Name = user });
 
-            Console.WriteLine("Greeter reply :" + reply.Message);
+                Console.WriteLine("Greeter reply :" + reply.Message);
 
-            var secReply = client.SayHello(new HelloRequest { Name = "lanbery" });
-            Console.WriteLine("Greeting: " + secReply.Message);
-            channel.ShutdownAsync().Wait();
+                var secReply = client.SayHello(new HelloRequest { Name = "lanbery" });
+                Console.WriteLine("Greeting: " + secReply.Message);
+                channel.ShutdownAsync().Wait();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Client connect channel failure. " + ex.Message);
+            }
+            
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 
--- rpc-svr/GreeterServer/Program.cs
+++ rpc-svr/GreeterServer/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Helloworld;
@@ -23,10 +25,30 @@ namespace GreeterServer
         const int Port = 50051;
         static void Main(string[] args)
         {
+            //localhost 
+            string hostName = Dns.GetHostName();
+            IPAddress[] iplist = Dns.GetHostAddresses(hostName);
+
+            foreach (IPAddress ipa in iplist)
+            {
+                if (ipa.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    Console.WriteLine(ipa.ToString());
+            }
+
+            if(IPAddressUtil.GetLocalIP() != null)
+            {
+                hostName = IPAddressUtil.GetLocalIP();
+                Console.WriteLine("Get Local IP :" + hostName);
+            }
+            else
+            {
+                hostName = "localhost";
+            }
+
             Server server = new Server
             {
                 Services = { Greeter.BindService(new GreeterImpl()) },
-                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                Ports = { new ServerPort(hostName, Port, ServerCredentials.Insecure) }
             };
 
             server.Start();
@@ -36,6 +58,93 @@ namespace GreeterServer
 
             string flag = Console.ReadKey().Key.ToString();
             server.ShutdownAsync().Wait();
+        }
+    }
+
+    /**
+     * 
+     */
+    class IPAddressUtil
+    {
+        const bool defaultLogged = false;
+        const string conndomain = "www.baidu.com";
+        public static string GetLocalIP()
+        {
+            string result = RunApp("route", "print", true);
+            System.Text.RegularExpressions.Match match = System.Text.RegularExpressions.Regex.Match(result, @"0.0.0.0\s+0.0.0.0\s+(\d+.\d+.\d+.\d+)\s+(\d+.\d+.\d+.\d+)");
+
+            if (match.Success)
+            {
+                return match.Groups[2].Value;
+            }else
+            {
+                try
+                {
+                    System.Net.Sockets.TcpClient cli = new System.Net.Sockets.TcpClient();
+                    cli.Connect(conndomain, 80);
+                    string ip = ((System.Net.IPEndPoint)cli.Client.LocalEndPoint).Address.ToString();
+                    cli.Close();
+                    return ip;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static string GetPrimaryDNS()
+        {
+            string result = RunApp("nslookup", "", true);
+
+            System.Text.RegularExpressions.Match match = System.Text.RegularExpressions.Regex.Match(result, @"\d+\.\d+\.\d+\.\d+");
+            if (match.Success)
+            {
+                return match.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static string RunApp(string filename,string arg,bool logged)
+        {
+            try
+            {
+                if (logged)
+                {
+                    Trace.WriteLine(filename + " " + arg);
+                }
+
+                Process proc = new Process();
+                proc.StartInfo.FileName = filename;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.Arguments = arg;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.Start();
+
+                using(System.IO.StreamReader sr = new System.IO.StreamReader(proc.StandardOutput.BaseStream, Encoding.Default))
+                {
+                    System.Threading.Thread.Sleep(100);
+                    if (!proc.HasExited)
+                    {
+                        proc.Kill();
+                    }
+                    string txt = sr.ReadToEnd();
+                    sr.Close();
+
+                    if (logged)
+                        Trace.WriteLine(txt);
+                    return txt;
+                }
+
+            }catch(Exception ex)
+            {
+                Trace.WriteLine(ex);
+                return ex.Message;
+            }
         }
     }
 }
